64df613796601f85e60f52cdb680af72ec333779
Rowland Wallerstein <wallersteinrowland@gmail.com>
2019-07-24 21:38:02 -0500

    no changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './App.css';
-import Contacts from './Contacts/Contacts';
 import Cpage from './Contacts/Cpage';
 import ContactList from './Contacts/ContactList';
 
@@ -15,14 +14,20 @@ class App extends Component {
 
   contactState = {data: ContactList}
 
+  clickHandler 
+
   render() {
   return (
     <div className="App">
       <header className="App-header">
         Contacts 'R' Us
       </header>
-      <div className='Contacts'>
-        <Contacts/>
+      <div className="Contact">
+          {this.contactState.data.map(d => <div className="Cinfo" onClick={this.clickHandler}>
+          <p className="Cname"> {d.name} </p>
+          <p className="Cphone">{d.phone}</p>
+          <p className="Cemail">{d.email}</p>
+          </div>)}
       </div>
       <div>
         <Cpage className="Cpage" name={this.pageState.name} phone={this.pageState.phone} email={this.pageState.email} desc={this.pageState.desc}/>
