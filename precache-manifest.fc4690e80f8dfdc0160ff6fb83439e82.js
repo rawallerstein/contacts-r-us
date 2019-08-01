d0a7d31e253304f42324a801fd38d2a38e4c6524
Rowland Wallerstein <wallersteinrowland@gmail.com>
2019-08-01 16:12:43 -0500

    Updates

---
+++ precache-manifest.fc4690e80f8dfdc0160ff6fb83439e82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359d53327b3061aa13b951a2ec775a73",
+    "url": "/rawallerstein/Contacts-R-Us-Deploy/index.html"
+  },
+  {
+    "revision": "ea6d3bacce43d3f1dc57",
+    "url": "/rawallerstein/Contacts-R-Us-Deploy/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d17c868faa0e742c681b",
+    "url": "/rawallerstein/Contacts-R-Us-Deploy/static/css/main.68d00f39.chunk.css"
+  },
+  {
+    "revision": "ea6d3bacce43d3f1dc57",
+    "url": "/rawallerstein/Contacts-R-Us-Deploy/static/js/2.400a4696.chunk.js"
+  },
+  {
+    "revision": "d17c868faa0e742c681b",
+    "url": "/rawallerstein/Contacts-R-Us-Deploy/static/js/main.4f96f816.chunk.js"
+  },
+  {
+    "revision": "601279cd21d741d430fa",
+    "url": "/rawallerstein/Contacts-R-Us-Deploy/static/js/runtime~main.8b0e7623.js"
+  }
+]);
